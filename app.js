b2ee42f545cc14d52ced4180fedef1a02a9b0fc7
littlewin <littlewin.wang@gmail.com>
2017-04-25 17:44:40 +0800

    增加http接口转发

--- app.js
+++ app.js
@@ -31,5 +31,15 @@ app.use(controller.get('/api_test', function*() {
   this.body = service.get_test_data();
 }));
 
+app.use(controller.get('/ajax/search', function*() {
+  this.set('Cache-Control', 'no-cache');
+  var querystring = require('querystring');
+  var params = querystring.parse(this.req._parsedUrl.query);
+  var start = params.start;
+  var end = params.end;
+  var keyword = params.keyword;
+  this.body = yield service.get_search_data(start, end, keyword);
+}));
+
 app.listen(3001);
 console.log('koa server is started!')
--- service/webAppService.js
+++ service/webAppService.js
@@ -4,3 +4,41 @@ exports.get_test_data = function() {
   var content = fs.readFileSync('./mock/test.json', 'utf-8');
   return content;
 }
+
+exports.get_search_data = function(start, end, keyword) {
+  return function(callback) {
+    var http = require('http');
+    var qs = require('querystring');
+    var data = {
+      s: keyword,
+      start: start,
+      end: end
+    };
+    var content = qs.stringify(data);
+    var http_request = {
+      hostname: 'dushu.xiaomi.com',
+      port: 80,
+      path: '/store/v0/lib/query/onebox?' + content,
+      method: 'GET'
+    }
+
+    req_obj = http.request(http_request, function(_res) {
+      var content = '';
+      _res.setEncoding('utf-8');
+      
+      _res.on('data', function(chunk){
+        content += chunk;
+      })
+
+      _res.on('end', function(e) {
+        callback(null, content)
+      })
+    });
+
+    req_obj.on('error', function() {
+
+    });
+
+    req_obj.end()
+  }
+}
