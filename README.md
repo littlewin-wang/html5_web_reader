9991494949ac883e5839aeb7fa3350f2fabda9ec
littlewin <littlewin.wang@gmail.com>
2017-04-26 11:22:10 +0800

    书城基本功能实现

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # html5_web_reader
-一个慕课网的实例教程-基于HTML5的WEB阅读器，后期本人做了一些优化
+一个慕课网的实例教程-基于HTML5的WEB阅读器
 
 ## Demo
 <img src="https://cloud.githubusercontent.com/assets/14028075/17583663/9d7a8de2-5fe4-11e6-9bc8-0f924db78d7e.gif" width="30%" height="30%">
 
-## Done
+## Feature
 
-- 页面架构
-- 触摸交互
-- 字体和页面背景设置交互
-- 页面数据获取
+- 基于NodeJS，Koa构建服务器运行环境
+- EJS服务端渲染
+- Vue数据驱动
+- 集成了HTML5 Web阅读器
 
-## 一些优化点
+## To do
 
-- 取消多余的字体和背景设置变量，直接用style中background-color和color属性来完成功能
-- 白天/黑夜模式切换过程会存储上一个背景，即点击按钮界面回到最近一次的白天/黑夜背景
+- 响应式设计，页面
+- 优化寻找合适的线上图书资源
 - To be continued
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = koa();
 
 var views = require('co-views');
 var render = views('./view', {
-  map: { html: 'ejs' }
+  map: {html: 'ejs'}
 });
 
 var koa_static = require('koa-static-server');
@@ -17,38 +17,70 @@ app.use(koa_static({
 
 var service = require('./service/webAppService');
 
-app.use(controller.get('/route_test', function*() {
+// app.use(controller.get('/route_test', function*() {
+//   this.set('Cache-Control', 'no-cache');
+//   this.body = 'Hello koa!';
+// }));
+//
+// app.use(controller.get('/ejs_test', function*() {
+//   this.set('Cache-Control', 'no-cache');
+//   this.body = yield render('test', { title: 'title_test'});
+// }));
+//
+// app.use(controller.get('/api_test', function*() {
+//   this.set('Cache-Control', 'no-cache');
+//   this.body = service.get_test_data();
+// }));
+
+// page routes
+app.use(controller.get('/', function*() {
   this.set('Cache-Control', 'no-cache');
-  this.body = 'Hello koa!';
+  this.body = yield render('index');
 }));
 
-app.use(controller.get('/ejs_test', function*() {
-  this.set('Cache-Control', 'no-cache');
-  this.body = yield render('test', { title: 'title_test'});
-}));
-
-app.use(controller.get('/api_test', function*() {
-  this.set('Cache-Control', 'no-cache');
-  this.body = service.get_test_data();
-}));
-
-app.use(controller.get('/', function*(){
-  this.set('Cache-Control', 'no-cache');
-  this.body = yield render('index', { title: '书城首页'});
-}));
-
-app.use(controller.get('/search', function*(){
-  this.set('Cache-Control', 'no-cache');
-  this.body = yield render('search', { title: '搜索首页'});
-}));
-
-app.use(controller.get('/book', function*(){
+app.use(controller.get('/book', function*() {
   this.set('Cache-Control', 'no-cache');
   var params = querystring.parse(this.req._parsedUrl.query);
-  var bookId = params.id
-  this.body = yield render('book', { bookId: bookId});
+  var bookId = params.id;
+  this.body = yield render('book', {nav: '书籍详情', bookId: bookId});
 }));
 
+app.use(controller.get('/search', function*() {
+  this.set('Cache-Control', 'no-cache');
+  this.body = yield render('search', {nav: '搜索'});
+}));
+
+app.use(controller.get('/reader', function*() {
+  this.set('Cache-Control', 'no-cache');
+  this.body = yield render('reader');
+}));
+
+app.use(controller.get('/male', function*() {
+  this.set('Cache-Control', 'no-cache');
+  this.body = yield render('male', {nav: '男生频道'});
+}));
+
+app.use(controller.get('/female', function*() {
+  this.set('Cache-Control', 'no-cache');
+  this.body = yield render('female', {nav: '女生频道'});
+}));
+
+app.use(controller.get('/usercenter', function*() {
+  this.set('Cache-Control', 'no-cache');
+  this.body = yield render('user-center', {nav: '用户中心'});
+}));
+
+app.use(controller.get('/rank', function*() {
+  this.set('Cache-Control', 'no-cache');
+  this.body = yield render('rank', {nav: '排行'});
+}));
+
+app.use(controller.get('/category', function*() {
+  this.set('Cache-Control', 'no-cache');
+  this.body = yield render('category', {nav: '分类'});
+}));
+
+// Mock api data get
 app.use(controller.get('/ajax/index', function*() {
   this.set('Cache-Control', 'no-cache');
   this.body = service.get_index_data();
@@ -63,25 +95,40 @@ app.use(controller.get('/ajax/book', function*() {
   this.set('Cache-Control', 'no-cache');
   var params = querystring.parse(this.req._parsedUrl.query);
   var id = params.id;
-  if(!id) {
+  if (!id) {
     id = '';
   }
   this.body = service.get_book_data(id);
 }));
 
-app.use(controller.get('/ajax/category', function*(){
+app.use(controller.get('/ajax/category', function*() {
   this.set('Cache-Control', 'no-cache');
   this.body = service.get_category_data();
 }));
 
-app.use(controller.get('/ajax/male', function*(){
+app.use(controller.get('/ajax/male', function*() {
   this.set('Cache-Control', 'no-cache');
   this.body = service.get_male_data();
 }));
 
-app.use(controller.get('/ajax/female', function*(){
+app.use(controller.get('/ajax/female', function*() {
   this.set('Cache-Control', 'no-cache');
   this.body = service.get_female_data();
+}));
+
+app.use(controller.get('/ajax/chapter', function*() {
+  this.set('Cache-Control', 'no-cache');
+  this.body = service.get_chapter_data();
+}));
+
+app.use(controller.get('/ajax/chapter_data', function*() {
+  this.set('Cache-Control', 'no-cache');
+  var params = querystring.parse(this.req._parsedUrl.query);
+  var id = params.id;
+  if (!id) {
+    id = "";
+  }
+  this.body = service.get_chapter_content_data(id);
 }));
 
 app.use(controller.get('/ajax/search', function*() {
@@ -93,5 +140,5 @@ app.use(controller.get('/ajax/search', function*() {
   this.body = yield service.get_search_data(start, end, keyword);
 }));
 
+// start server
 app.listen(3001);
-console.log('koa server is started!')
